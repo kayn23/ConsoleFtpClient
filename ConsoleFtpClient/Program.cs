d1349330a9114bb74fe3aeca09697ad08dc1e54a
anatoly <kayn23@yandex.ru>
2020-11-29 11:22:45 +0300

    Init

---
+++ ConsoleFtpClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFtpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
